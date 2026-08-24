74467bd4e50cd4f45453b60bfcd13357a44c582d
Hetvi Prajapati <hetviprajapati114@gmail.com>
2026-08-25 00:17:52 +0530

    coming soon page upadated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,15 @@ import './page.css';
 export default function Home() {
   return (
     <div className="coming-soon">
-      <h1>Coming soon</h1>
+      <div className="coming-soon-inner">
+        <span className="h1">rmssd.com</span>
+        <span className="span">Live autonomic measurement.</span>
+      </div>
+      <div className="coming-soon-inner">
+        <span className="span">We are in private beta.</span>
+        <span className="span">Public launch: Fall 2026.</span>
+      </div>
+      <span className="span">lab@rmssd.com</span>
     </div>
   );
 }
